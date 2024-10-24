5fd2d512a2b088d6c304b0034eb35fe8207cfad1
Hututu-byte <2831818119@qq.com>
2024-10-24 15:41:32 +0800

    init ： 初始后端项目创建

---
+++ src/main/java/com/example/developerassessmentsmaster/DeveloperAssessmentsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.developerassessmentsmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperAssessmentsMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperAssessmentsMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/developerassessmentsmaster/DeveloperAssessmentsMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.developerassessmentsmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperAssessmentsMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
